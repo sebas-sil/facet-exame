bdd0dd12c2072b3709e0e9f98c781738a655b785
Your Name <you@example.com>
2019-04-10 21:06:33 -0300

    foi feito um aclasse pessoa e uma classe pessoateste

---
+++ src/br/facet/exame1/Pessoa.java
@@ -0,0 +1,18 @@
+package br.facet.exame1;
+public class Pessoa {
+	
+	String nome;
+	int data_de_nascimento;
+	int cpf;
+	
+	
+	 void falarIdade() {
+		System.out.println("35");
+	}
+	
+	 public static void main(String[] args) {
+		
+	}
+
+}
+ 
--- src/br/facet/exame1/PessoaTeste.java
+++ src/br/facet/exame1/PessoaTeste.java
@@ -1,8 +1,11 @@
 package br.facet.exame1;
 
-public class Escola {
+public class PessoaTeste {
 	public static void main(String[] args) {
-		
+		Pessoa p = new Pessoa();
+		p.falarIdade();
 		
 	}
+	
+	
 }
